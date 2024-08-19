16f1dd1562432e3ec99234139af3b5100f85b1b0
ShashwatkumarXD <shashwat200418@gmail.com>
2024-08-19 22:27:28 +0530

    Learning React-Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import store from './app/store'
+import { Provider } from 'react-redux'
+import { Counter } from './features/counter/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    {/* <App /> */}
+    <Counter/>
+    </Provider>
 );
 
