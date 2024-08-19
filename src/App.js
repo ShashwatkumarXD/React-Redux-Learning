c3c794210a2f8460ee5bc009736659f8d0886d5f
ShashwatkumarXD <shashwat200418@gmail.com>
2024-08-19 21:13:56 +0530

    Learning React-Redux

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [count,setCount] = useState(0)
+
+  function incr(){
+    setCount(count + 1)
+  }
+
+  function decr(){
+    setCount(count - 1)
+  }
+
+
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incr}>+</button>
+      <button onClick={decr}>-</button>
+    </div>
+  )
+}
+
+export default App
